9d6ee8aa1f30373984a8335bf8a77d44fc345e66
Spencer Cumming <sc4368@nyu.edu>
2016-09-22 15:51:06 -0400

    Shark Detective 2nd try, only 580 files

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+	bool didPlayerWin = false;
+	public Text display;
+	public Transform player;
+	public Transform goal;
+	public GameObject captain;
+	public Transform first;
+	public Transform second;
+	public Transform third;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (didPlayerWin) {
+			display.text = "Uh... guess you got a little hungry..." +
+			"Oh well! \nAnother case cracked by the peerless SHARK DETECTIVE!!";
+		} else if ((goal.position - player.position).magnitude < 5f) {
+			display.text = "You found the Captain! Press [SPACE] to put him \nin your mouth" +
+			" and bring him safely home! Wait a minute....";
+			if (Input.GetKey (KeyCode.Space)) {
+				didPlayerWin = true;
+				captain.SetActive (false);
+			}
+		} else if ((first.position - player.position).magnitude < 5f) {
+			display.text = "Some shipping crates containing rum from the wreck!" +
+			"\n You decide to follow the scent to the northeast!";
+		} else if ((second.position - player.position).magnitude < 5f) {
+			display.text = "Oars from the lifeboats! \nWhy would they leave these behind?" +
+			" \nYou hear something to the south!";
+		} else if ((third.position - player.position).magnitude < 5f) {
+			display.text = "The life raft! The Captain must be close! \nYou smell blood to the west.";
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public int moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3 (moveSpeed, 0, 0) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += new Vector3 (0, 0, moveSpeed) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, -90, 0);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position += new Vector3 (-moveSpeed, 0, 0) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, 180, 0);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3 (0, 0, -moveSpeed) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, 90, 0);
+		}
+		Camera.main.transform.position = transform.position + new Vector3 (-6, 4, 0);
+
+	}
+}
